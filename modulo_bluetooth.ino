9bed966ade9d1c78d02a2dc42c06a4b8238c3682
Eldavis14-lab <84482211+Eldavis14-lab@users.noreply.github.com>
2023-09-30 05:51:14 -0600

    Update modulo_bluetooth.ino

--- modulo_bluetooth.ino
+++ modulo_bluetooth.ino
@@ -1,17 +1,21 @@
 #include <SoftwareSerial.h>
-#include <LiquidCrystal_I2C.h> //DESCARGAR LIBRERÍA:https://github.com/ELECTROALL/Codigos-arduino/blob/master/LiquidCrystal_I2C.zip
+#include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2); // si no te sale con esta direccion  puedes usar (0x3f,16,2) || (0x27,16,2)  ||(0x20,16,2) 
+// Configuración de la dirección I2C del LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Si no funciona con esta dirección, puedes probar con (0x3f, 16, 2) || (0x27, 16, 2) || (0x20, 16, 2)
 
+// Configuración de los pines RX y TX para el módulo Bluetooth
 SoftwareSerial bluetoothSerial(2, 3); // RX, TX
 
 void setup() {
-  Serial.begin(9600);     
-   lcd.init();
+  // Inicialización de la comunicación serial del Arduino Nano
+  Serial.begin(9600);
+
+  // Inicialización de la pantalla LCD
+  lcd.init();
   lcd.backlight();
-  lcd.clear();// Inicializamos la comunicación serial del Arduino Nano
-  bluetoothSerial.begin(9600);  // Inicializamos la comunicación serial del módulo Bluetooth HC-06
-  
+  lcd.clear();
+ bluetoothSerial.begin(9600);
   // Esperamos un momento para que el módulo Bluetooth se inicie completamente
   delay(1000);
 }
@@ -19,15 +23,16 @@ void setup() {
 void loop() {
   // Verificamos si hay datos disponibles en el módulo Bluetooth
   if (bluetoothSerial.available()) {
-   lcd.setCursor(0,0);
-  lcd.print(conectado); 
+    lcd.setCursor(0, 0);
+    lcd.print("CONECTADO");
+
     // Leemos el mensaje completo desde el módulo Bluetooth
     String receivedMessage = bluetoothSerial.readStringUntil('\n');
-    
+
     // Imprimimos el mensaje recibido en el monitor serial
     Serial.print("Mensaje recibido desde Bluetooth: ");
     Serial.println(receivedMessage);
-    
+
     // Puedes agregar aquí la lógica que desees para procesar los datos recibidos desde el Bluetooth
   }
 
@@ -35,7 +40,7 @@ void loop() {
   if (Serial.available()) {
     // Leemos el mensaje completo desde el monitor serial
     String sendMessage = Serial.readStringUntil('\n');
-    
+
     // Enviaremos el mensaje completo al módulo Bluetooth
     bluetoothSerial.println(sendMessage);
   }
